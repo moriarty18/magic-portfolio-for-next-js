de05ddc30c75e7a2dd186b89d71e2fea78093f4f
moriarty18 <65659817+moriarty18@users.noreply.github.com>
2024-12-24 13:47:52 +0500

    Update config.js

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -1,12 +1,12 @@
 const baseURL = 'demo.magic-portfolio.com'
 
 // Enable localization
-const i18n = false;
+const i18n = true;
 
 // Manage localized content in the messages folder
 const i18nOptions = {
-    locales: ['en'],            // A list of all locales that are supported, e.g. ['en','id']
-    defaultLocale: 'en'         // Locale used by default and as a fallback
+    locales: ['en'],['ru']            // A list of all locales that are supported, e.g. ['en','id']
+    defaultLocale: 'ru'         // Locale used by default and as a fallback
 }
 
 const routes = {
