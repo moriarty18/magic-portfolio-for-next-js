6916ea4cd7a1cbcd238927d987421a2c3076195c
moriarty18 <65659817+moriarty18@users.noreply.github.com>
2025-02-15 12:57:57 +0500

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -13,6 +13,8 @@ import { renderContent } from "@/app/resources";
 import { Background, Flex } from "@/once-ui/components";
 import { Analytics } from '@vercel/analytics/next';
 
+const GTM_ID = 'GTM-WCRPDD5Z';
+
 export async function generateMetadata(
     { params: { locale }}: { params: { locale: string }}
 ) {
@@ -96,6 +98,19 @@ export default async function RootLayout({
                     tertiary ? tertiary.variable : '',
                     code.variable)}>
                 <Script
+                    id="gtm-script"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','${GTM_ID}');
+                        `,
+                    }}
+                />
+                <Script
                     async
                     src="https://www.googletagmanager.com/gtag/js?id=AW-16874881298"
                     strategy="afterInteractive"
@@ -112,6 +127,14 @@ export default async function RootLayout({
                     as="body"
                     fillWidth margin="0" padding="0"
                     direction="column">
+                    <noscript>
+                        <iframe 
+                            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                            height="0"
+                            width="0"
+                            style={{ display: 'none', visibility: 'hidden' }}
+                        />
+                    </noscript>
                     <Background
                         mask={effects.mask as any}
                         gradient={effects.gradient as any}
